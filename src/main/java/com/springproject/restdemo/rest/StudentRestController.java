de5083eb8f1ef22b696017bd697993cbf9809489
phsu40 <phsu40@gatech.edu>
2023-04-14 16:12:05 -0700

    Refactor students list with PostConstruct so it only loads once

--- src/main/java/com/springproject/restdemo/rest/StudentRestController.java
+++ src/main/java/com/springproject/restdemo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.springproject.restdemo.rest;
 
 import com.springproject.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,21 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> students;
 
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData() {
         // create sample students
-        List<Student> students = new ArrayList<>();
+        students = new ArrayList<>();
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return students;
     }
